feb259d866641c0f4777554cc02c8d95b017d131
Ariela Cohen <arielaades@gmail.com>
2019-07-22 19:48:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     return array.join("")
   when 2
     return array.join("and")
-  when >2
+  else
   index = array.size-1
   new_array = array.insert(index, "and")
   string = new_array.join(", ")
