0efc5b3a816cccfd8c1002e5df26f1866317e7ad
Ariela Cohen <arielaades@gmail.com>
2019-07-22 19:45:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,9 @@
 def oxford_comma(array)
   index = array.size-1
   new_array = array.insert(index, "and")
-  new_array.sub! "and," , "and"
+  string = new_array.join(", ")
+  return string.sub! "and," , "and"
 
-  return new_array
+
 
 end
